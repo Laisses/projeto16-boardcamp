2387c7ad901d528d3ea78daa068d838b244bec31
Laisses <laisse@gmail.com>
2022-12-07 09:11:34 -0300

    src/routes.js: create get /categories route

--- src/routes.js
+++ src/routes.js
@@ -2,5 +2,8 @@ import * as handlers from "./handlers.js";
 import * as middlewares from "./middlewares.js";
 
 export const routes = (app) => {
+
+    app.get("/categories", middlewares.asyncError(handlers.selectCategories));
+
     app.get("/games", middlewares.asyncError(handlers.selectGames));
 };
