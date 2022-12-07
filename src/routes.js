159a88e4992347ef61df9aa41801a88fab4cdde3
Laisses <laisse@gmail.com>
2022-12-07 08:10:37 -0300

    src/routes.js: add get /games route

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,8 @@
+import * as handlers from "./handlers.js";
+import * as middlewares from "./middlewares.js";
+
 export const routes = (app, connection) => {
 
-    app.get("/games", async (req, res) => {
-
-        try {
-            const games = await connection.query("SELECT * FROM games");
-            res.send(games.rows);
-        } catch(err) {
-            console.log(err);
-        }
-
-    });
+    app.get("/games", middlewares.asyncError(handlers.selectGames));
 
 };
