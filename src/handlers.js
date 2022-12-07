ca768bb1b4772d819fad1a2f68af1b45e197e839
Laisses <laisse@gmail.com>
2022-12-07 09:14:59 -0300

    src/handlers.js: create select all categories handler

--- src/handlers.js
+++ src/handlers.js
@@ -1,5 +1,10 @@
 import { connection } from "./database.js";
 
+export const selectCategories = async (_req, res) => {
+    const categories = await connection.query("SELECT * FROM categories");
+    res.status(200).send(categories.rows);
+};
+
 export const selectGames = async (_req, res) => {
     const games = await connection.query("SELECT * FROM games");
     res.status(200).send(games.rows);
