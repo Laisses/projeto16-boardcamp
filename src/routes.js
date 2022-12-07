48964ff55601fe59a122e17148abcaff6d85165f
Laisses <laisse@gmail.com>
2022-12-07 09:28:57 -0300

    src/routes.js: create post /categories route

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import * as middlewares from "./middlewares.js";
 export const routes = (app) => {
 
     app.get("/categories", middlewares.asyncError(handlers.selectCategories));
+    app.post("/categories", middlewares.asyncError(handlers.addCategory));
 
     app.get("/games", middlewares.asyncError(handlers.selectGames));
 };
