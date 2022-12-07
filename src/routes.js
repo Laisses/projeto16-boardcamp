fd823c58f0eaf250105111bfcb00dd91b4a7f8bd
Laisses <laisse@gmail.com>
2022-12-07 18:04:42 -0300

    src/routes.js: create get /customers route

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ export const routes = (app) => {
     app.get("/categories", middlewares.asyncError(handlers.selectCategories));
     app.post("/categories", middlewares.asyncError(handlers.addCategory));
 
+    app.get("/customers", middlewares.asyncError(handlers.selectCustomers));
+
     app.get("/games", middlewares.asyncError(handlers.selectGames));
     app.post("/games", middlewares.asyncError(handlers.addGame));
 };
