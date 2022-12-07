3386bc7b0bd174d894700f7101d17616782f473c
Laisses <laisse@gmail.com>
2022-12-07 07:56:00 -0300

    src/routes.js: create get /games route

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,14 @@
-export const routes = () => {
+export const routes = (app, connection) => {
+
+    app.get("/games", async (req, res) => {
+
+        try {
+            const games = await connection.query("SELECT * FROM games");
+            res.send(games.rows);
+        } catch(err) {
+            console.log(err);
+        }
+
+    });
 
 };
