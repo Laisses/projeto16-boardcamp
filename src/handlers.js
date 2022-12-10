adfe557c3f4bb36f97406fc3c0cb6e2ab5c41de7
Laisses <laisse@gmail.com>
2022-12-09 21:24:53 -0300

    src/handlers.js: create add new rental handler

--- src/handlers.js
+++ src/handlers.js
@@ -196,3 +196,21 @@ export const selectRentals = async (req, res) => {
 
     res.status(200).send(rentals);
 };
+
+export const addRental = async (req, res) => {
+    const { customerId, gameId, daysRented } = req.body;
+    const date = new Date();
+
+    const price = await connection.query(`SELECT "pricePerDay" FROM games WHERE id=$1;`, [gameId]);
+
+    const originalPrice = price.rows[0].pricePerDay * daysRented;
+
+    await connection.query(`
+        INSERT INTO
+            rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee")
+        VALUES
+            ($1, $2, $3, $4, $5, $6, $7)
+    ;`, [customerId, gameId, date, daysRented, null, originalPrice, null]);
+
+    res.sendStatus(201);
+};
