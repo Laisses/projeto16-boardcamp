cd05b6341c4d6cf57ee6326e6c592427f4877c59
Laisses <laisse@gmail.com>
2022-12-11 19:23:31 -0300

    src/routes.js: add category schema validation

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import * as s from "./schemas.js";
 export const routes = (app) => {
 
     app.get("/categories", m.validateQueryParams, m.asyncError(handlers.selectCategories));
-    app.post("/categories", m.asyncError(handlers.addCategory));
+    app.post("/categories", m.validate(s.category), m.asyncError(handlers.addCategory));
 
     app.get("/customers", m.validateQueryParams, m.asyncError(handlers.selectCustomers));
     app.get("/customers/:id", m.asyncError(handlers.selectCustomer));
