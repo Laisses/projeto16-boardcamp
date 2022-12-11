910f51ef164bdf2d174b4621964db94eeaab19dc
Laisses <laisse@gmail.com>
2022-12-11 19:17:37 -0300

    add schema validation to customers POST and PUT

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import * as handlers from "./handlers.js";
 import * as m from "./middlewares.js";
+import * as s from "./schemas.js";
 
 export const routes = (app) => {
 
@@ -8,8 +9,8 @@ export const routes = (app) => {
 
     app.get("/customers", m.validateQueryParams, m.asyncError(handlers.selectCustomers));
     app.get("/customers/:id", m.asyncError(handlers.selectCustomer));
-    app.post("/customers", m.validateNewCustomer, m.asyncError(handlers.addCustomer));
-    app.put("/customers/:id", m.validateUpdatedCustomer, m.asyncError(handlers.updateCustomer));
+    app.post("/customers", m.validate(s.user), m.validateNewCustomer, m.asyncError(handlers.addCustomer));
+    app.put("/customers/:id", m.validate(s.user), m.validateUpdatedCustomer, m.asyncError(handlers.updateCustomer));
 
     app.get("/games", m.validateQueryParams, m.asyncError(handlers.selectGames));
     app.post("/games", m.asyncError(handlers.addGame));
